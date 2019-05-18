e6ffc30ddca3ef44e5930816d15043f7b5c28dad
angelacelar <angelacelar@gmail.com>
2019-05-18 15:27:11 +0200

    Initial

---
+++ db/access.php
@@ -0,0 +1,23 @@
+<?php
+$capabilities = array (
+	'block/angela:myaddinstance' => array(
+		'captype' => 'write',
+		'contextlevel' => CONTEXT_SYSTEM,
+		'archetypes' => array(
+			'user' => CAP_ALLOW
+		),
+		'clonepermissionsfrom' => 'moodle/my:manageblocks'
+	),
+	
+	'block/angela:addinstance' => array(
+		'riskbitmask' => RISK_SPAM | RISK_XSS,
+		'captype' => 'write',
+		'contextlevel' => CONTEXT_BLOCK,
+		'archetypes' => array(
+			'editingteacher' => CAP_ALLOW,
+			'manager' => CAP_ALLOW
+		),
+		'clonepermissionsfrom' => 'moodle/site:manageblocks'
+	),
+);
+?>
