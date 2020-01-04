4fc3f04bd078a569ea38e44ef603c6299d4fb186
Angela Čelar <39595704+angelacelar@users.noreply.github.com>
2020-01-04 13:24:12 +0100

    Add files via upload

---
+++ lang/en/block_la_dashboard.php
@@ -0,0 +1,12 @@
+<?php
+$string['pluginname'] = 'LA dashboard';
+$string['myplugin'] = 'Nadzorna ploča analitike učenja';
+$string['myplugin:addinstance'] = 'Dodaj Nadzornu ploču analitike učenja';
+$string['myplugin:myaddinstance'] ='Dodaj na moju moodle stranicu Nadzornu ploču analitike učenja';
+$string['grades_chart'] = 'Tablica ocjena';
+$string['statistics'] = 'Pogledaj statistiku';
+$string['la_dashboardsettings'] = 'Postavke bloka';
+$string['chart_view'] = 'Nadzorna ploča analitike učenja';
+$string['pogledaj_stanje'] = 'Pogledaj predviđanje i stanje ocjena';
+$string['predvidi_student'] = 'Predvidi moju ocjenu';
+$string['predvidi_teacher'] = 'Predvidi ocjene studenata';
