0faeee167e4e6a6acbbcca040754fa7d029de916
Angela Čelar <39595704+angelacelar@users.noreply.github.com>
2020-01-04 13:24:30 +0100

    Delete block_angela.php

--- lang/en/block_angela.php
+++
@@ -1,6 +0,0 @@
-<?php
-$string['pluginname'] = 'Angela plugin';
-$string['angela'] = 'Angela'; //ovaj naziv poziva funkcija get_string u prvoj php datoteci
-$string['myplugin:addinstance'] = 'Add new Angela block';
-$string['myplugin:myaddinstance'] = 'Add new Angela block to the My Moodle page';
-?>
